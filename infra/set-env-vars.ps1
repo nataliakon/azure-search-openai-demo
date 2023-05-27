d43432638efaebdc5f1d5b2e931fc800d43f2b22
nataliakon <nataliak@microsoft.com>
2023-05-27 23:29:55 +0000

    update the env set param script

---
+++ infra/set-env-vars.ps1
@@ -0,0 +1,12 @@
+azd env set AZURE_ENV_NAME azmin45
+azd env set AZURE_LOCATION eastus
+azd env set AZURE_VNET_SUBSCRIPTION_ID f013bc3f-1407-4b68-a977-9f22ef438677
+azd env set AZURE_VNET_NAME data-vnet-es
+azd env set AZURE_VNET_RESOURCE_GROUP rg-hub-networking
+azd env set AZURE_PRIVATE_ENDPOINT_SUBNET private-endpoint-subnet
+azd env set AZURE_COMPUTE_SUBNET private-endpoint-subnet
+azd env set AZURE_APP_SERVICE_SUBNET appServiceSubnet
+azd env set AZURE_PRIVATE_DNS_SUBSCRIPTION_ID f013bc3f-1407-4b68-a977-9f22ef438677
+azd env set AZURE_PRIVATE_DNS_RESOURCE_GROUP rg-dns
+azd env set AZURE_DSVM_ADMIN_USERNAME azadmin
+azd env set AZURE_DSVM_ADMIN_PASSWORD
